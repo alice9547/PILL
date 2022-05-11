36fb41d99f16dd3d2fe93d4d5c9a2d45d18cc376
김은서 <kes@quantec.co.kr>
2022-05-11 16:57:20 +0900

    Storyboard - VC 연결

---
+++ Pill/ResultDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  ResultDetailVC.swift
+//  Pill
+//
+//  Created by 김은서 on 2022/05/11.
+//
+
+import UIKit
+
+class ResultDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
