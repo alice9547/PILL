cefc62d1f6c445cbe8b26ff25d9808780247e4fa
김은서 <kes@quantec.co.kr>
2022-05-11 16:32:19 +0900

    UIView @IBInspectable _ 인스팩터에서 기본적인거 조절가능

---
+++ Pill/DesignExtenstion.swift
@@ -0,0 +1,49 @@
+//
+//  DesignExtenstion.swift
+//  Pill
+//
+//  Created by 김은서 on 2022/05/11.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+  
+  @IBInspectable
+  var cornerRadius: CGFloat {
+    get {
+      return layer.cornerRadius
+    }
+    set {
+      layer.cornerRadius = newValue
+    }
+  }
+  
+  @IBInspectable
+  var borderWidth: CGFloat {
+    get {
+      return layer.borderWidth
+    }
+    set {
+      layer.borderWidth = newValue
+    }
+  }
+  
+  @IBInspectable
+  var borderColor: UIColor? {
+    get {
+      if let color = layer.borderColor {
+        return UIColor(cgColor: color)
+      }
+      return nil
+    }
+    set {
+      if let color = newValue {
+        layer.borderColor = color.cgColor
+      } else {
+        layer.borderColor = nil
+      }
+    }
+  }
+}
