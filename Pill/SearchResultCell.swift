0bb95cf9ec352c7baa4c8a90a446330dda565e88
김은서 <kes@quantec.co.kr>
2022-06-02 18:41:41 +0900

    data 추가

---
+++ Pill/SearchResultCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResultCell.swift
+//  Pill
+//
+//  Created by 김은서 on 2022/06/02.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
