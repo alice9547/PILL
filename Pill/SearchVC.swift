236d300bf23b3c5ab6462c663a77fe650567e0d4
김은서 <kes@quantec.co.kr>
2022-06-02 18:41:54 +0900

    사진 추가

--- Pill/SearchVC.swift
+++ Pill/SearchVC.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
-class SearchVC: UIViewController {
+class SearchVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var tfName: UITextField!
-    @IBOutlet weak var tfSymptom: UITextField!
+    @IBOutlet weak var tfEfficacy: UITextField!
+    @IBOutlet weak var preview: UIImageView!
     @IBOutlet weak var btnSearch: UIButton!
     
     override func viewDidLoad() {
@@ -19,17 +20,43 @@ class SearchVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func btnSerch(_ sender: Any) {
+    @IBAction func pick(_ sender: Any) {
+        let alert = UIAlertController(title: "이미지를 가져올 곳을 선택해주세요", message: "", preferredStyle: .actionSheet)
+        
+        let camera = UIAlertAction(title: "카메라", style: .default){(_) in
+            self.setAction(type: .camera)
+        }
+        
+        let album = UIAlertAction(title: "저장앨범", style: .default){(_) in
+            self.setAction(type: .savedPhotosAlbum)
+        }
+        
+        let library = UIAlertAction(title: "사진 라이브러리", style: .default) {(_) in
+            self.setAction(type: .photoLibrary)
+        }
+        
+        //메시지 창 컨트롤러에 버튼 액션을 추가
+        alert.addAction(camera)
+        alert.addAction(album)
+        alert.addAction(library)
+        
+        //메시지 창 컨트롤러를 표시
+        self.present(alert, animated: false)
+        
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setAction(type:UIImagePickerController.SourceType){
+        let picker = UIImagePickerController()
+        
+        picker.sourceType = type
+        picker.delegate = self
+        picker.allowsEditing = true
+        self.present(picker, animated: false)
     }
-    */
-
+   
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        self.preview.image = info[.editedImage] as? UIImage
+        
+        picker.dismiss(animated: false)
+    }
 }
